dfc3c180841520c43741e6958d71304d314feedb
Edward <e.kovalchuk@tut.by>
2022-12-02 06:46:32 +0300

    02122022 0646 Monoblock

--- 001 TASKS/TASKS/01-variable-declaration/start.js
+++ 001 TASKS/TASKS/01-variable-declaration/start.js
@@ -32,3 +32,38 @@ const myFunc = function (param) {return param + '_'}
 
 console.log(`Меня зовут ${myFunc(name)}, я живу в городе ${city}`)
 
+setTimeout(() => console.log('111'), 5000);
+
+try {
+    const a = 5
+    a = 6
+} catch (error) {
+    //console.error(error) 
+    console.log(error.message) 
+}
+
+console.log('ok')
+
+try {
+    throw new Error('error!!')
+} catch (error) {
+    //console.error(error) 
+    console.log(error.message) 
+}
+
+console.log('ok')
+
+const newObject = (obj, upDate = Date()) => ({
+    ...obj,
+    upDate,
+})
+
+const originObject = {
+    user: 'Edward',
+    email: 'ke@tut.by',
+}
+
+const getNew = newObject(originObject)
+
+console.table(originObject)
+console.table(getNew)
